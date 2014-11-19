8f005da4a852a454cdd3f2f6d1530a48b2521045
Raffael Thome <mail@rmt.name>
2014-11-19 18:32:37 +0100

    Delete unneeded macros from Util.hpp

--- src/Util.hpp
+++ src/Util.hpp
@@ -9,11 +9,6 @@
 #define LOG_WARN(msg) do { std::cout << "[WARN] [" << clock() << "] " << msg << std::endl; } while(0);
 #define LOG_ERROR(msg) do { std::cout << "[ERROR] [" << clock() << "] ["  << __FILE__ << ":" << __LINE__ << "] " << msg << std::endl; } while(0);
 
-// Logging macros for InstanceID-derived classes
-#define LOG_INFO_ID(msg) LOG_INFO("(ID=" << uniqueObjectID() << ") " << msg)
-#define LOG_WARN_ID(msg) LOG_WARN("(ID=" << uniqueObjectID() << ") " << msg)
-#define LOG_ERROR_ID(msg) LOG_ERROR("(ID=" << uniqueObjectID() << ") " << msg)
-
 // Assertion macros
 #define CC_ASSERT(cond) assert(cond);
 #define CC_STATIC_ASSERT(cond) static_assert(cond);
